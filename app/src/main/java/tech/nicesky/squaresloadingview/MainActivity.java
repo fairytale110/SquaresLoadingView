2843103aaf1c41b08b05d86b01552add244625f2
Anand Deshmukh <ananddeshmukh001@gmail.com>
2019-10-10 00:48:16 +0530

    Migrated to AndroidX

--- app/src/main/java/tech/nicesky/squaresloadingview/MainActivity.java
+++ app/src/main/java/tech/nicesky/squaresloadingview/MainActivity.java
@@ -1,7 +1,7 @@
 package tech.nicesky.squaresloadingview;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import tech.nicesky.libsquaresloadingview.SquaresLoadingView;
